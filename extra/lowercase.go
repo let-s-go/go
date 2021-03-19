4ba1a11de6cdaa334c5a2165de3150c37dde71d1
let-s-go <reborn1576@163.com>
2021-03-19 08:59:59 +0800

    fix lower case bug

--- extra/lowercase.go
+++ extra/lowercase.go
@@ -1,34 +1,20 @@
 package extra
 
 import (
-	"strings"
 	"unicode"
-
-	"github.com/let-s-go/jsoniter"
 )
 
-func init() {
-	jsoniter.RegisterExtension(&lowercaseNamingExtension{})
+func SetLowerNamingStrategy() {
+	SetNamingStrategy(LowerCase)
 }
 
-type lowercaseNamingExtension struct {
-	jsoniter.DummyExtension
-}
-
-func (extension *lowercaseNamingExtension) UpdateStructDescriptor(structDescriptor *jsoniter.StructDescriptor) {
-	for _, binding := range structDescriptor.Fields {
-		tags := strings.Split(binding.Field.Tag.Get("json"), ",")
-		if len(tags) == 0 || tags[0] == "" {
-			binding.ToNames = []string{extension.toLowerCase(binding.Field.Name)}
-			binding.FromNames = []string{extension.toLowerCase(binding.Field.Name)}
+func LowerCase(name string) string {
+	rs := []rune(name)
+	for i, r := range rs {
+		if unicode.IsLower(r) {
+			break
 		}
+		rs[i] = unicode.ToLower(r)
 	}
-}
-
-func (extension *lowercaseNamingExtension) toLowerCase(name string) string {
-	newName := []rune(name)
-	if len(newName) > 0 {
-		newName[0] = unicode.ToLower(newName[0])
-	}
-	return string(newName)
+	return string(rs)
 }
